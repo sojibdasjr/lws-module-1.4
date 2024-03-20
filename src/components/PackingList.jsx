d9b4d5d90837b542e1970665ac921e93c62676f0
sojibdasjr <contact.sojibdas@gmail.com>
2024-03-21 01:42:56 +0600

    Conditional Rendering

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,13 +1,18 @@
 function Item({ name, isPacked }) {
-  return <li className="item">{name}</li>;
+  // if (isPacked) {
+  //   return <li className="item">{name} ✔</li>;
+  // }
+  return (
+    <li className="item"> {isPacked ? <del>{name + "✔"}</del> : name} </li>
+  );
 }
 
 export default function PackingList() {
   return (
     <section>
-      <h1>Sally Rides Packing List</h1>
+      <h1>Sally Ride's Packing List</h1>
       <ul>
-        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={false} name="Space suit" />
         <Item isPacked={true} name="Helmet with a golden leaf" />
         <Item isPacked={false} name="Photo of Tam" />
       </ul>
