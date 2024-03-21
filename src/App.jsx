02cbeae75a3071ad1b091b35a354552581434641
sojibdasjr <contact.sojibdas@gmail.com>
2024-03-21 20:51:52 +0600

    Filter and map Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Bio from "./components/Bio";
 import Gallery, { Profile } from "./components/Gallery";
 import ChalangeThree from "./components/JavaScriptInJSXWithCurllyBracess/ChallengeThree";
+import ListItem from "./components/ListItem";
 import PackingList from "./components/PackingList";
 import Styleing from "./components/Styleing";
 import Test from "./components/Test";
@@ -31,6 +32,8 @@ function App() {
       <ChalangeThree />
 
       <PackingList />
+
+      <ListItem />
     </>
   );
 }
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
+
+const ListItem = () => {
+  const chemists = people.filter((chemist) => chemist.profession === "chemist");
+  const listPeople = chemists.map((chemist) => <li>{chemist.name}</li>);
+  return (
+    <div>
+      <ul>{listPeople}</ul>
+    </div>
+  );
+};
+
+export default ListItem;
